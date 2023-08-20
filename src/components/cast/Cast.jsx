b2867c738176f9409ae2fba5e15eb7081e674113
Kostyantyn <kosta.rusin@gmail.com>
2023-08-20 09:55:49 +0200

    cast reviews

--- src/components/cast/Cast.jsx
+++ src/components/cast/Cast.jsx
@@ -1,42 +1,64 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { fetchMovieCast } from 'API';
+import Loader from 'components/loader/Loader';
 
 const Cast = () => {
   const [casts, setCasts] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [error, setError] = useState(null);
   const { movieId } = useParams();
   const defaultImg = `https://i.pinimg.com/564x/01/0c/b2/010cb2c89b8924d966b4369c0142a6cd.jpg`;
 
   useEffect(() => {
     if (!movieId) return;
     fetchMovieCast(movieId)
-      .then(data => {
-        setCasts(data.cast);
+      .then(({ cast }) => {
+        if (cast.length === 0) {
+          setIsEmpty(true);
+          return;
+        }
+        setCasts(cast);
       })
       .catch(error => {
-        console.error(error);
-      });
+        setError(error.message);
+      })
+      .finally(setIsloading(false));
   }, [movieId]);
 
   return (
-    <ul>
-      {casts &&
-        casts.map(cast => (
-          <li key={cast.id}>
-            <img
-              src={
-                cast.profile_path
-                  ? `https://image.tmdb.org/t/p/w200${cast.profile_path}`
-                  : defaultImg
-              }
-              width={250}
-              alt={`${cast.name}`}
-            />
-            {!cast.profile_path && <p>Sorry, we didn't find a photo.</p>}
-            {cast.name}
-          </li>
-        ))}
-    </ul>
+    <>
+      {isLoading && <Loader />}
+      {isEmpty && <p>Sorry, there is no cast.</p>}
+      {error && (
+        <p
+          style={{
+            fontSize: 'large',
+          }}
+        >
+          Sorry. {error} 😭
+        </p>
+      )}
+      <ul>
+        {casts &&
+          casts.map(cast => (
+            <li key={cast.id}>
+              <img
+                src={
+                  cast.profile_path
+                    ? `https://image.tmdb.org/t/p/w200${cast.profile_path}`
+                    : defaultImg
+                }
+                width={250}
+                alt={`${cast.name}`}
+              />
+              {!cast.profile_path && <p>Sorry, we didn't find a photo.</p>}
+              {cast.name}
+            </li>
+          ))}
+      </ul>
+    </>
   );
 };
 
--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,32 +1,54 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { fetchMovieReview } from 'API';
+import Loader from 'components/loader/Loader';
 
 const Rewiews = () => {
   const [reviews, setReviews] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsloading] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
   const { movieId } = useParams();
 
   useEffect(() => {
     if (!movieId) return;
     fetchMovieReview(movieId)
-      .then(data => {
-        setReviews(data.results);
+      .then(({ results }) => {
+        if (results.length === 0) {
+          setIsEmpty(true);
+          return;
+        }
+        setReviews(results);
       })
       .catch(error => {
-        console.error(error);
-      });
+        setError(error.message);
+      })
+      .finally(setIsloading(false));
   }, [movieId]);
 
   return (
-    <ul>
-      {reviews &&
-        reviews.map(review => (
-          <li key={review.id}>
-            <p>{review.author}</p>
-            {review.content}
-          </li>
-        ))}
-    </ul>
+    <>
+      {isLoading && <Loader />}
+      {isEmpty && <p>Sorry, there is no reviews.</p>}
+      {error && (
+        <p
+          style={{
+            fontSize: 'large',
+          }}
+        >
+          Sorry. {error} 😭
+        </p>
+      )}
+      <ul>
+        {reviews &&
+          reviews.map(review => (
+            <li key={review.id}>
+              <p>{review.author}</p>
+              {review.content}
+            </li>
+          ))}
+      </ul>
+    </>
   );
 };
 
