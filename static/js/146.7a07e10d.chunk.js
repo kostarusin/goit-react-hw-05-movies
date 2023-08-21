bb5e4d69a0fd2f6a54a51aad87a328a080d64d2e
kostarusin <122443715+kostarusin@users.noreply.github.com>
2023-08-21 15:10:32 +0000

    Deploying to gh-pages from @ kostarusin/goit-react-hw-05-movies@7f7c3285df13a55bfbe3dc148749a26389ff3a34 🚀

--- static/js/146.7a07e10d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[146],{146:function(e,t,n){n.r(t);var r=n(184);t.default=function(e){var t=e.onSearchSubmit,n=e.selectedMovies,a=e.onQueryChange;return(0,r.jsx)("div",{style:{margin:"20px 0"},children:(0,r.jsxs)("form",{onSubmit:t,children:[(0,r.jsx)("input",{type:"text",value:n,onChange:a}),(0,r.jsx)("button",{type:"submit",children:"Search"})]})})}}}]);
-//# sourceMappingURL=146.7a07e10d.chunk.js.map
