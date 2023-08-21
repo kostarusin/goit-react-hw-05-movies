7f7c3285df13a55bfbe3dc148749a26389ff3a34
Kostyantyn <kosta.rusin@gmail.com>
2023-08-21 17:09:34 +0200

    components deleted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,25 @@
-import { lazy } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import SharedLayout from 'components/sharedlayout/SharedLayout';
+// import { lazy } from 'react';
+// import { Navigate, Route, Routes } from 'react-router-dom';
+// import SharedLayout from 'components/SharedLayout/SharedLayout';
 
-const Home = lazy(() => import('pages/Home'));
-const Movies = lazy(() => import('pages/Movies'));
-const MovieDetails = lazy(() => import('pages/MovieDetails'));
-const Cast = lazy(() => import('components/Cast/Cast'));
-const Reviews = lazy(() => import('components/Reviews/Reviews'));
+// const Home = lazy(() => import('pages/Home'));
+// const Movies = lazy(() => import('pages/Movies'));
+// const MovieDetails = lazy(() => import('pages/MovieDetails'));
+// const Cast = lazy(() => import('components/Cast/Cast'));
+// const Reviews = lazy(() => import('components/Reviews/Reviews'));
 
-export const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Route>
-    </Routes>
-  );
-};
+// export const App = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<SharedLayout />}>
+//         <Route index element={<Home />} />
+//         <Route path="/movies" element={<Movies />} />
+//         <Route path="/movies/:movieId" element={<MovieDetails />}>
+//           <Route path="cast" element={<Cast />} />
+//           <Route path="reviews" element={<Reviews />} />
+//         </Route>
+//         <Route path="*" element={<Navigate to="/" />} />
+//       </Route>
+//     </Routes>
+//   );
+// };
